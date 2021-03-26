a3e655942ad4240c59ed5fa2397689036e2650a2
Jacqueline Teixeira <jackyfialho@hotmail.com>
2021-03-26 09:20:10 -0300

    lesson 42

---
+++ api/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '7a064be0e931bce00cb2ebb0723b08d0'),
+    },
+  },
+});
---
+++ api/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'cb51b8a7-af7f-4bd2-868f-21e94a3805bf'
+};
